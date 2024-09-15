16691d723c8fcb1cc789a6987aeb3033dc3e301a
NehaChawdipande <nehachawdipande10@gmail.com>
2024-09-15 13:36:14 +0530

    added test for comma seperated numbers

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -10,3 +10,8 @@ test('should return 0 for an empty string', () => {
 test('should return the same number for a single number', () => {
     expect(calculate('5')).toBe(5);
 });
+
+// Test case 3: Multiple numbers separated by commas
+test('should add multiple numbers separated by commas', () => {
+    expect(calculate('1,2,3')).toBe(6);
+});
